889bd472c4778025f1a0792eabe3eb2ebc594a7d
Ajit Tiwari <ajittiwari65@gmail.com>
2020-08-18 12:35:30 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+//Prompt to select media stream
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        console.log("Error:", error);
+    }
+}
+
+button.addEventListener('click', async () => {
+    button.disabled = true;
+    await videoElement.requestPictureInPicture();
+    button.disabled = false;
+});
+
+//On Load
+selectMediaStream()
